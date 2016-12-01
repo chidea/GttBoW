55420447868cfed057ad3be49476aa25972b9f12
chidea <sbw228@gmail.com>
2016-12-01 12:20:29 +0900

    batch

--- batch/sbash.bat
+++ batch/sbash.bat
@@ -1,5 +1,10 @@
 @echo off
-bash -c "%* >/mnt/c/Temp/_tmpout 2>/mnt/c/Temp/_tmperr"
+
+echo %* | set /p=%* > c:\Temp\_tmpin
+echo ^>/mnt/c/Temp/_tmpout 2^>/mnt/c/Temp/_tmperr >> c:\Temp\_tmpin
+bash /mnt/c/Temp/_tmpin
+del c:\Temp\_tmpin
+
 if exist c:\Temp\_tmpout (
   type c:\Temp\_tmpout
   del c:\Temp\_tmpout
