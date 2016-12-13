ba89f780b14b6f574da0fab748cdf6a39211028f
chidea <sbw228@gmail.com>
2016-12-13 22:57:08 +0900

    ubuntu package installation added into install.bat

--- install.bat
+++ install.bat
@@ -3,5 +3,6 @@
 go build gbash.go ssh-agent.go
 move gbash.exe %windir%\system32\
 copy batch\*.bat %windir%\system32\
+bbash sudo apt install git curl wget gcc-mingw-w64-x86-64 -y
 
 echo GttBoW Installation succeed.
