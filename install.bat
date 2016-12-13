412cf18a4a2eef265a9718bd5a87fe7928d56ad2
chidea <sbw228@gmail.com>
2016-12-13 22:59:30 +0900

    use normal mode as default installation

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-go build gbash.go ssh-agent.go
+go build gbash.go normal.go
 move gbash.exe %windir%\system32\
 copy batch\*.bat %windir%\system32\
 bbash sudo apt install git curl wget gcc-mingw-w64-x86-64 -y
